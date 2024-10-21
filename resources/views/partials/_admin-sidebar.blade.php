a24d1850e605498e966f3601aeb7ebe97b1cf598
Shuga8 <lotocharles8@gmail.com>
2024-10-21 16:42:07 +0100

    uploading

--- resources/views/partials/_admin-sidebar.blade.php
+++ resources/views/partials/_admin-sidebar.blade.php
@@ -11,19 +11,19 @@
 
         <a href="{{ route('admin.dashboard') }}"
             class="sidebar-anchor py-3 px-3 hover:bg-white hover:bg-opacity-10  w-full rounded-md flex flex-row gap-x-2"><span
-                class="material-symbols-outlined text-sm">
+                class="material-symbols-outlined text-xs" style="font-size: 0.75rem;line-height: 1rem;">
                 dashboard
-            </span> <span class="text-sm">Dashboard</span>
+            </span> <span class="text-xs">Dashboard</span>
         </a>
 
         <div class="dropdown-list" data-url="/admin/users">
             <div class="dropdownBtn py-3 px-3 flex flex-row justify-between place-items-center text-white  hover:bg-white hover:bg-opacity-10 p-1 rounded-md cursor-pointer"
                 onclick="toggleDropDown(0)">
                 <p class="flex flex-row gap-x-2">
-                    <span class="material-symbols-outlined text-sm">
+                    <span class="material-symbols-outlined text-xs" style="font-size: 0.75rem;line-height: 1rem;">
                         group
                     </span>
-                    <span class="text-sm">Manage Users</span>
+                    <span class="text-xs">Manage Users</span>
                 </p>
 
                 <span class="material-symbols-outlined arrow-icon">
@@ -35,7 +35,7 @@
                 <a href="{{ route('admin.users.active') }}"
                     class="py-3 px-3 w-full flex flex-row justify-between place-items-center hover:bg-white hover:bg-opacity-10">
                     <p class="flex flex-row gap-x-2">
-                        <span class="material-symbols-outlined text-xs">
+                        <span class="material-symbols-outlined text-xs" style="font-size: 0.75rem;line-height: 1rem;">
                             radio_button_checked
                         </span>
                         </span> <span class="text-xs">Active Users</span>
@@ -47,7 +47,7 @@
                 <a href="{{ route('admin.users.banned') }}"
                     class="py-3 px-3 w-full flex flex-row justify-between place-items-center hover:bg-white hover:bg-opacity-10">
                     <p class="flex flex-row gap-x-2">
-                        <span class="material-symbols-outlined text-xs">
+                        <span class="material-symbols-outlined text-xs" style="font-size: 0.75rem;line-height: 1rem;">
                             radio_button_checked
                         </span>
                         </span> <span class="text-xs">Banned Users</span>
@@ -59,7 +59,7 @@
                 <a href="{{ route('admin.users.all') }}"
                     class="py-3 px-3 w-full flex flex-row justify-between place-items-center hover:bg-white hover:bg-opacity-10">
                     <p class="flex flex-row gap-x-2">
-                        <span class="material-symbols-outlined text-xs">
+                        <span class="material-symbols-outlined text-xs" style="font-size: 0.75rem;line-height: 1rem;">
                             radio_button_checked
                         </span>
                         </span> <span class="text-xs">All Users</span>
@@ -74,26 +74,26 @@
 
         <a href="{{ route('admin.investment.all') }}"
             class="sidebar-anchor py-3 px-3 hover:bg-white hover:bg-opacity-10 w-full rounded-md flex flex-row gap-x-2"><span
-                class="material-symbols-outlined text-sm">
+                class="material-symbols-outlined text-xs" style="font-size: 0.75rem;line-height: 1rem;">
                 finance
-            </span> <span class="text-sm">Manage Investment Plans</span>
+            </span> <span class="text-xs">Manage Investment Plans</span>
         </a>
 
         <a href="{{ route('admin.wallet.all') }}"
             class="sidebar-anchor py-3 px-3 hover:bg-white hover:bg-opacity-10 w-full rounded-md flex flex-row gap-x-2"><span
-                class="material-symbols-outlined text-sm">
+                class="material-symbols-outlined text-xs" style="font-size: 0.75rem;line-height: 1rem;">
                 wallet
-            </span> <span class="text-sm">Manage Wallet</span>
+            </span> <span class="text-xs">Manage Wallet</span>
         </a>
 
         <div class="dropdown-list" data-url="/admin/investments">
             <div class="dropdownBtn py-3 px-3 flex flex-row justify-between place-items-center text-white  hover:bg-white hover:bg-opacity-10 p-1 rounded-md cursor-pointer"
                 onclick="toggleDropDown(1)">
                 <p class="flex flex-row gap-x-2">
-                    <span class="material-symbols-outlined text-sm">
+                    <span class="material-symbols-outlined text-xs" style="font-size: 0.75rem;line-height: 1rem;">
                         finance_mode
                     </span>
-                    <span class="text-sm">Investments</span>
+                    <span class="text-xs">Investments</span>
                 </p>
 
                 <span class="material-symbols-outlined arrow-icon">
@@ -105,7 +105,7 @@
                 <a href="{{ route('admin.users.investments', 0) }}"
                     class="py-3 px-3 w-full flex flex-row justify-between place-items-center hover:bg-white hover:bg-opacity-10">
                     <p class="flex flex-row gap-x-2">
-                        <span class="material-symbols-outlined text-xs">
+                        <span class="material-symbols-outlined text-xs" style="font-size: 0.75rem;line-height: 1rem;">
                             radio_button_checked
                         </span>
                         </span> <span class="text-xs">Running Investments</span>
@@ -118,7 +118,7 @@
                 <a href="{{ route('admin.users.investments', 1) }}"
                     class="py-3 px-3 w-full flex flex-row justify-between place-items-center hover:bg-white hover:bg-opacity-10">
                     <p class="flex flex-row gap-x-2">
-                        <span class="material-symbols-outlined text-xs">
+                        <span class="material-symbols-outlined text-xs" style="font-size: 0.75rem;line-height: 1rem;">
                             radio_button_checked
                         </span>
                         </span> <span class="text-xs">Completed Investments</span>
@@ -131,7 +131,7 @@
                 <a href="{{ route('admin.users.investments', 'all') }}"
                     class="py-3 px-3 w-full flex flex-row justify-between place-items-center hover:bg-white hover:bg-opacity-10">
                     <p class="flex flex-row gap-x-2">
-                        <span class="material-symbols-outlined text-xs">
+                        <span class="material-symbols-outlined text-xs" style="font-size: 0.75rem;line-height: 1rem;">
                             radio_button_checked
                         </span>
                         </span> <span class="text-xs">All Investments</span>
@@ -149,10 +149,10 @@
             <div class="dropdownBtn py-3 px-3 flex flex-row justify-between place-items-center text-white  hover:bg-white hover:bg-opacity-10 p-1 rounded-md cursor-pointer"
                 onclick="toggleDropDown(2)">
                 <p class="flex flex-row gap-x-2">
-                    <span class="material-symbols-outlined text-sm">
+                    <span class="material-symbols-outlined text-xs" style="font-size: 0.75rem;line-height: 1rem;">
                         mintmark
                     </span>
-                    <span class="text-sm">Manage Deposits</span>
+                    <span class="text-xs">Manage Deposits</span>
                 </p>
 
                 <span class="material-symbols-outlined arrow-icon">
@@ -164,7 +164,7 @@
                 <a href="{{ route('admin.users.deposits', 'pending') }}"
                     class="py-3 px-3 w-full flex flex-row justify-between place-items-center hover:bg-white hover:bg-opacity-10">
                     <p class="flex flex-row gap-x-2">
-                        <span class="material-symbols-outlined text-xs">
+                        <span class="material-symbols-outlined text-xs" style="font-size: 0.75rem;line-height: 1rem;">
                             radio_button_checked
                         </span>
                         </span> <span class="text-xs">Pending Deposits</span>
@@ -176,7 +176,7 @@
                 <a href="{{ route('admin.users.deposits', 'approved') }}"
                     class="py-3 px-3 w-full flex flex-row justify-between place-items-center hover:bg-white hover:bg-opacity-10">
                     <p class="flex flex-row gap-x-2">
-                        <span class="material-symbols-outlined text-xs">
+                        <span class="material-symbols-outlined text-xs" style="font-size: 0.75rem;line-height: 1rem;">
                             radio_button_checked
                         </span>
                         </span> <span class="text-xs">Approved Deposits</span>
@@ -188,7 +188,7 @@
                 <a href="{{ route('admin.users.deposits', 'rejected') }}"
                     class="py-3 px-3 w-full flex flex-row justify-between place-items-center hover:bg-white hover:bg-opacity-10">
                     <p class="flex flex-row gap-x-2">
-                        <span class="material-symbols-outlined text-xs">
+                        <span class="material-symbols-outlined text-xs" style="font-size: 0.75rem;line-height: 1rem;">
                             radio_button_checked
                         </span>
                         </span> <span class="text-xs">Rejected Deposits</span>
@@ -200,7 +200,7 @@
                 <a href="{{ route('admin.users.deposits', 'all') }}"
                     class="py-3 px-3 w-full flex flex-row justify-between place-items-center hover:bg-white hover:bg-opacity-10">
                     <p class="flex flex-row gap-x-2">
-                        <span class="material-symbols-outlined text-xs">
+                        <span class="material-symbols-outlined text-xs" style="font-size: 0.75rem;line-height: 1rem;">
                             radio_button_checked
                         </span>
                         </span> <span class="text-xs">All Deposits</span>
@@ -218,10 +218,10 @@
             <div class="dropdownBtn py-3 px-3 flex flex-row justify-between place-items-center text-white  hover:bg-white hover:bg-opacity-10 p-1 rounded-md cursor-pointer"
                 onclick="toggleDropDown(3)">
                 <p class="flex flex-row gap-x-2">
-                    <span class="material-symbols-outlined text-sm">
+                    <span class="material-symbols-outlined text-xs" style="font-size: 0.75rem;line-height: 1rem;">
                         send_money
                     </span>
-                    <span class="text-sm">Manage Withdrawals</span>
+                    <span class="text-xs">Manage Withdrawals</span>
                 </p>
 
                 <span class="material-symbols-outlined arrow-icon">
@@ -234,7 +234,7 @@
                 <a href="{{ route('admin.users.withdrawals', 'pending') }}"
                     class="py-3 px-3 w-full flex flex-row justify-between place-items-center hover:bg-white hover:bg-opacity-10">
                     <p class="flex flex-row gap-x-2">
-                        <span class="material-symbols-outlined text-xs">
+                        <span class="material-symbols-outlined text-xs" style="font-size: 0.75rem;line-height: 1rem;">
                             radio_button_checked
                         </span>
                         </span> <span class="text-xs">Pending Withdrawals</span>
@@ -247,7 +247,7 @@
                 <a href="{{ route('admin.users.withdrawals', 'approved') }}"
                     class="py-3 px-3 w-full flex flex-row justify-between place-items-center hover:bg-white hover:bg-opacity-10">
                     <p class="flex flex-row gap-x-2">
-                        <span class="material-symbols-outlined text-xs">
+                        <span class="material-symbols-outlined text-xs" style="font-size: 0.75rem;line-height: 1rem;">
                             radio_button_checked
                         </span>
                         </span> <span class="text-xs">Approved Withdrawals</span>
@@ -260,7 +260,7 @@
                 <a href="{{ route('admin.users.withdrawals', 'rejected') }}"
                     class="py-3 px-3 w-full flex flex-row justify-between place-items-center hover:bg-white hover:bg-opacity-10">
                     <p class="flex flex-row gap-x-2">
-                        <span class="material-symbols-outlined text-xs">
+                        <span class="material-symbols-outlined text-xs" style="font-size: 0.75rem;line-height: 1rem;">
                             radio_button_checked
                         </span>
                         </span> <span class="text-xs">Rejected Withdrawals</span>
@@ -272,7 +272,7 @@
                 <a href="{{ route('admin.users.withdrawals', 'all') }}"
                     class="py-3 px-3 w-full flex flex-row justify-between place-items-center hover:bg-white hover:bg-opacity-10">
                     <p class="flex flex-row gap-x-2">
-                        <span class="material-symbols-outlined text-xs">
+                        <span class="material-symbols-outlined text-xs" style="font-size: 0.75rem;line-height: 1rem;">
                             radio_button_checked
                         </span>
                         </span> <span class="text-xs">All Withdrawals</span>
@@ -288,34 +288,37 @@
 
         <a href="{{ route('admin.settings') }}"
             class="sidebar-anchor py-3 px-3 hover:bg-white hover:bg-opacity-10 w-full rounded-md flex flex-row gap-x-2"><span
-                class="material-symbols-outlined text-sm">
+                class="material-symbols-outlined text-xs" style="font-size: 0.75rem;line-height: 1rem;">
                 engineering
-            </span> <span class="text-sm">Settings</span>
+            </span> <span class="text-xs">Settings</span>
         </a>
 
         <a
             class="py-3 px-3 hover:bg-white hover:bg-opacity-10 w-full rounded-md flex flex-row justify-between gap-x-2 cursor-pointer">
-            <span class="flex flex-row  gap-x-2"><span class="material-symbols-outlined text-sm">
+            <span class="flex flex-row  gap-x-2"><span class="material-symbols-outlined text-xs"
+                    style="font-size: 12px;">
                     notifications
-                </span> <span class="text-sm">Notifications</span>
+                </span> <span class="text-xs">Notifications</span>
             </span>
             <span class="p-1 bg-red-600 text-xs rounded-sm upgrade-span">upgrade</span>
         </a>
 
         <a
             class="py-3 px-3 hover:bg-white hover:bg-opacity-10 w-full rounded-md flex flex-row justify-between gap-x-2 cursor-pointer">
-            <span class="flex flex-row  gap-x-2"><span class="material-symbols-outlined text-sm">
+            <span class="flex flex-row  gap-x-2"><span class="material-symbols-outlined text-xs"
+                    style="font-size: 12px;">
                     mail_lock
-                </span> <span class="text-sm">Mail</span>
+                </span> <span class="text-xs">Mail</span>
             </span>
             <span class="p-1 bg-red-600 text-xs rounded-sm upgrade-span">upgrade</span>
         </a>
 
         <a
             class="py-3 px-3 hover:bg-white hover:bg-opacity-10 w-full rounded-md flex flex-row justify-between gap-x-2 cursor-pointer">
-            <span class="flex flex-row  gap-x-2"><span class="material-symbols-outlined text-sm">
+            <span class="flex flex-row  gap-x-2"><span class="material-symbols-outlined text-xs"
+                    style="font-size: 12px;">
                     manufacturing
-                </span> <span class="text-sm">Site Settings</span>
+                </span> <span class="text-xs">Site Settings</span>
             </span>
             <span class="p-1 bg-red-600 text-xs rounded-sm upgrade-span">upgrade</span>
         </a>
@@ -323,9 +326,9 @@
 
         <a href="{{ route('admin.logout') }}"
             class="py-3 px-3 w-full rounded-md flex flex-row gap-x-2 hover:bg-white hover:bg-opacity-10"><span
-                class="material-symbols-outlined text-sm">
+                class="material-symbols-outlined text-xs" style="font-size: 0.75rem;line-height: 1rem;">
                 logout
-            </span> <span class="text-sm">Logout</span>
+            </span> <span class="text-xs">Logout</span>
         </a>
 
 
--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,7 @@ import laravel from "laravel-vite-plugin";
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                "resources/css/app.css",
-                "resources/js/app.js",
-                "public/app.css",
-                "public/user.scss",
-            ],
+            input: ["resources/css/app.css", "resources/js/app.js"],
             refresh: true,
         }),
     ],
