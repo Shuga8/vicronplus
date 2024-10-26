3d2ffe6ba75aadf096002d5048f8a0ab43b21d32
Shuga8 <lotocharles8@gmail.com>
2024-10-27 00:34:14 +0100

    subscription mail and job, minor changes...

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Models\Subscriber;
+use App\Jobs\SubcriptionJob;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\FileController;
-use App\Http\Controllers\LanguageController;
 use App\Http\Controllers\PagesController;
+use Illuminate\Support\Facades\Validator;
+use App\Http\Controllers\LanguageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,8 +26,21 @@ Route::get('/contact-us', [PagesController::class, 'contact'])->name('contact');
 Route::post('/send-mail', function () {
     return back()->with(['success' => 'Message recieved']);
 });
-Route::post('subscribe', function () {
-    return back()->with(['success' => 'Subscription successful']);
+Route::post('subscribe', function (Request $request) {
+
+    $validator = Validator::make($request->all(), ['email' => ['required', 'string', 'email', 'unique:subscribers,email']]);
+
+    if ($validator->fails()) {
+        return back()->with(['error' => $validator->errors()->first()]);
+    }
+
+    $subscriber = new Subscriber();
+    $subscriber->email = $request->input('email');
+    $subscriber->save();
+
+    dispatch(new SubcriptionJob($request->input('email')));
+
+    return back()->with(['success' => 'your subscribtion is being reviewed, please check your mail.']);
 })->name('subscribe');
 Route::get('storage/{folder}/{img}', [FileController::class, 'show']);
 Route::get('lang', [LanguageController::class, 'index'])->name('getLang');
